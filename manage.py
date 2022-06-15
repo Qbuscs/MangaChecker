78a13833ed31a8f5f4bd0edc1ec43a7ba3dd505f
unknown <kubawarzyn@gmail.com>
2022-06-16 00:32:05 +0200

    working prototype

--- manage.py
+++ manage.py
@@ -8,7 +8,7 @@ from manga.manga_fox import MangaFox
 class MangaAlreadyAddedException(Exception):
     pass
 
-def _get_manga_list() -> List[Manga]:
+def get_manga_list() -> List[Manga]:
     with open("manga/manga_progress.json", "r") as file:
         manga_json = file.read()
     manga_list_raw = json.loads(manga_json)
@@ -22,13 +22,13 @@ def _get_manga_list() -> List[Manga]:
     
     return manga_list
 
-def _save_manga_list(manga_list: List[Manga]) -> None:
+def save_manga_list(manga_list: List[Manga]) -> None:
     with open("manga/manga_progress.json", "w") as file:
         manga_dict_list = [m.to_dict() for m in manga_list]
         file.write(json.dumps(manga_dict_list, indent=4, separators=(", ", ": ")))
 
 def add_manga(name: str, url: str, website: str) -> None:
-    manga_list = _get_manga_list()
+    manga_list = get_manga_list()
     if name in [m.name for m in manga_list]:
         raise MangaAlreadyAddedException
     if website == Manga.MANGA_FOX:
@@ -36,11 +36,11 @@ def add_manga(name: str, url: str, website: str) -> None:
 
     new_manga.update()
     manga_list.append(new_manga)
-    _save_manga_list(manga_list)
+    save_manga_list(manga_list)
     
 
 def update_manga() -> Tuple[bool, List[str]]:
-    manga_list = _get_manga_list()
+    manga_list = get_manga_list()
     errors = []
     updated = False
     for manga in manga_list:
@@ -51,13 +51,23 @@ def update_manga() -> Tuple[bool, List[str]]:
             errors.append(f"Connection error while trying to reach {manga.link}")
         except PageParseException:
             errors.append(f"Error while parsing page {manga.link}")
-    _save_manga_list(manga_list)
+    save_manga_list(manga_list)
     return updated, errors
 
 def remove_manga(name: str) -> None:
-    manga_list = _get_manga_list()
+    manga_list = get_manga_list()
     for i, manga in enumerate(manga_list):
         if manga.name == name:
             manga_list.pop(i)
-    _save_manga_list(manga_list)
-    
+    save_manga_list(manga_list)
+
+def set_as_read(name: str) -> None:
+    manga_list = get_manga_list()
+    for manga in manga_list:
+        if manga.name == name:
+            manga.read = True
+    save_manga_list(manga_list)
+
+def read_file_binary(path: str) -> bytes:
+    with open(path, "rb") as f:
+        return f.read()
--- update_thread.py
+++ update_thread.py
@@ -7,16 +7,22 @@ from manage import update_manga
 class UpdateThread(threading.Thread):
     _UPDATE_INTERVAL = 10
 
+    def __init__(self, action):
+        super().__init__()
+        self.action = action
+
     def run(self):
         while True:
-            self.run_updates()
             time.sleep(self._UPDATE_INTERVAL)
+            self.run_updates()
 
     def run_updates(self) -> None:
-        updated, errors = update_manga()
-        if updated:
-            print("Updated")
-        else:
-            print("No new updates")
-        for error in errors:
-            print(error)
+        print("Updating...")
+        try:
+            updated, errors = update_manga()
+            if updated:
+                self.action()
+            for error in errors:
+                print(error)
+        except Exception as e:
+            print(e)
